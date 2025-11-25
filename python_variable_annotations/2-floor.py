6dc9590db99b1b6bd96647e19aa7cca7273b9be9
sokolgj19 <gjekasokol19@gmail.com>
2025-11-25 10:43:34 +0000

    corrected code and imported math

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """A module that returns the floor of a float"""
 
+import math
+
 
 def floor(n: float) -> float:
     """Returns the floor of a float"""
-    return n // 2
+    return math.floor(n)
