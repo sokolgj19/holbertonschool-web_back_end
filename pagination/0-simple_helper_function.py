e391e9b84f3e63612743c00d252284574da0730f
sokolgj19 <gjekasokol19@gmail.com>
2025-12-02 10:31:13 +0000

    added import typing from Tuple

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,7 +2,7 @@
 """Simple helper function for pagination index calculation."""
 from typing import Tuple
 
-def index_range(page: int, page_size: int)-> Tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Return a tuple (start, end) for pagination based on page and page_size."""
     start = (page - 1) * page_size
     end = start + page_size
