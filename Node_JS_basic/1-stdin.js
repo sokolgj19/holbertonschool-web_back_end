9aa6ccdf84f1dc241939e3a9f26296b23bd661d4
sokolgj19 <gjekasokol19@gmail.com>
2026-01-15 11:07:20 +0100

    program named 1-stdin.js that will be executed through a specific command line

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,9 @@
-#!/usr/bin/env node
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  if (name) {
-    process.stdout.write(`Your name is: ${name}\n`);
-  }
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
